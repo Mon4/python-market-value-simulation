679c567d84c4803976842dab67700ff43b125990
Monika Etrych <monika.etrych@gmail.com>
2024-05-01 13:54:19 +0200

    .

--- market_value/model.py
+++ market_value/model.py
@@ -1,1 +1,29 @@
+from keras import Sequential
 from tensorflow.keras.layers import LSTM
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+tf.random.set_seed(7)
+
+
+# normalize
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# split data
+
+# model
+model = Sequential()
+model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1))
+model.compile(loss='mae', optimizer='adam')
+
+history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+
+# plot
